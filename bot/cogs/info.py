0f705532844b367752424637c1954709e97799ee
yak-fumblepack <mike.liang2006@gmail.com>
2021-01-01 00:30:02 -0500

    Happy new year! 2021!

--- bot/cogs/info.py
+++ bot/cogs/info.py
@@ -1,8 +1,41 @@
 import discord
 from discord.ext import commands
 
+
 class Info(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        
+    
+    # displays ready message on load
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Info cog has been loaded sucessfully')
+
+    # singular server info command 
+    @commands.command(aliases=["servinfo", "sinfo"], no_pm=True)
+    async def serverinfo(self, ctx):
+        author = ctx.message.author
+        server = ctx.message.guild
+        guildicon = server.icon_url
+
+
+        emb = discord.Embed(title=str(server), color=0x9966cc)
+        emb.set_thumbnail(url=guildicon)
+        emb.add_field(name="Server: ", value=server.name)
+        emb.add_field(name="Owner: ", value=server.owner, inline=False)
+        emb.add_field(name="Member count: ", value=server.member_count, inline=True)
+        emb.add_field(name="Online members: ", value=sum(member.status!=discord.Status.offline and not member.bot for member in server.members), inline=False)
+        emb.add_field(name="Voice channels: ", value=str(len([j for j in server.channels if type(j) == discord.channel.TextChannel])), inline=False)
+        emb.add_field(name="Voice channels: ", value=str(len([j for j in server.channels if type(j) == discord.channel.VoiceChannel])), inline=False)
+        emb.add_field(name="Emoji count:", value=str(len(server.roles)), inline=False)
+        emb.add_field(name="Role count:", value=str(len(server.emojis)), inline=False)
+
+        try:
+            await ctx.send(author, embed=emb)
+        except:
+            await ctx.send("Sorry there was an error processing this command")
+
+
+def setup(bot):
+    bot.add_cog(Info(bot))
--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,63 @@
-from discord.ext import commands
+import os
 
 import discord
-import os
 import requests
+from discord.ext import commands
+from dotenv import load_dotenv
+
+# basic settings
+load_dotenv()
+token = os.getenv("token")
+
+bot = commands.Bot(command_prefix=';;')
+bot.remove_command("help")
+
+cogs = [
+    'cogs.info',
+    ]
+
+for cog in cogs:
+    try:
+        bot.load_extension(cog)
+    except Exception as e:
+        print(f'Could not load cog {cog}: {str(e)}')
+
+
+
+
+# main 
+
+@bot.event
+async def on_ready():
+    print('Bot is running')
+
+# load and unload cogs for modularity purposes 
+@bot.command()
+@commands.is_owner()
+async def loadcog(ctx, cogname=None):
+    if cogname == None:
+        return
+    try:
+        bot.load_extension(cogname)
+    except Exception as e:
+        print(f'Could not load cog {cogname}: {str(e)}')
+    else: 
+        print(f'Loaded cog: {cogname} sucessfully')
+
+@bot.command()
+@commands.is_owner()
+async def unloadcog(ctx, cogname=None):
+    if cogname == None:
+        return
+    try:
+        bot.unload_extension(cogname)
+    except Exception as e:
+        print(f'Could not unload cog {cogname}: {str(e)}')
+    else: 
+        print(f'Unloaded cog: {cogname} sucessfully')
+
+
+
+
+
+bot.run(token)
