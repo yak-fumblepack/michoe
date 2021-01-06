a55fd8e82d15a773dae3fbb406f7d8030cdb4692
yak-fumblepack <mike.liang2006@gmail.com>
2021-01-06 17:10:16 -0500

    Release admin file and basic commands

---
+++ bot/cogs/admin.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+
+class Admin(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Admin cog has been loaded sucessfully')
+
+    @commands.command()
+    async def say(self, ctx, dm_type=None, dm_id=None, *, args=None):
+        if dm_type == 'channel' and dm_id!=None:
+            try: 
+                target = await bot.fetch_channel(dm_id)
+                await target.send(args)
+            except:
+                await ctx.send("Error could not dm the channel")
+        else:
+            await ctx.send("Failed to specify channel, channelid")
+    
+    @commands.command()
+    @commands.check_any(commands.is_owner(), commands.has_permissions(administrator=True), commands.has_permissions(manage_messages=True), commands.has_role("william?"))
+    async def dm(self, ctx, dm_type=None, dm_id=None, *, args=None):
+        if dm_type == 'user' and dm_id!=None:
+            try: 
+                target = await bot.fetch_user(dm_id)
+                await target.send(args)
+            except:
+                await ctx.send("Error could not dm the user")
+        else:
+            await ctx.send("Failed to specify user, userid or you're lacking the permission")
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
